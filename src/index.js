7ca7228f170f5425969d267da25fe1619c0d82ab
gingkapls <73906888+gingkapls@users.noreply.github.com>
2024-05-04 18:20:41 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+window.console.log('hello world');
+
+const hello = () => {
+  // TODO:
+};
+
+// for linting testing
+class Test {
+  #mem1;
+
+  #mem2;
+
+}
---
+++ webpack.common.js
@@ -0,0 +1,51 @@
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hello World',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            cacheDirectory: true,
+            cacheCompression: true,
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
